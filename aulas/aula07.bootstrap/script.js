718beea56a34807bd8b9bdfca394d9f7c969ed37
Anderson Dutra <128000302+AndersonDutra7@users.noreply.github.com>
2023-09-14 19:29:37 -0300

    14.09

---
+++ aulas/aula07.bootstrap/script.js
@@ -0,0 +1,22 @@
+// var x;
+// let y;
+// const z;
+
+let texto = 'String'
+let inteiro = 20
+let decimal = 20.5
+let boleano = true
+let lista = ['a', 'q', 'c']
+let objeto = {
+    'nome': 'Anderson',
+    'email' : 'a@gmail.com'
+}
+let nulo = null
+let indefinido = undefined
+
+console.log(lista[3]);
+console.log(objeto['email']);
+inteiro.toString();
+
+
+
